e792d942e56fb077248e98faa9ba73f352cb64ee
James <jameswj111@gmail.com>
2025-05-12 20:21:06 -0400

    finishes multiple filter functionality of select component

--- components/Filter.jsx
+++ components/Filter.jsx
@@ -1,27 +1,93 @@
 const Filter = (props) => {
-    console.log('filter mounted');
-  return (
-    <div>
-      {Object.keys(props.startFilters).map((filter, index) => (
-        <select key={index} name={filter} onChange={(event) => {
-            props.setFilterValue({k: event.target.name, v: event.target.value})
-        }}>
-          <option key={"default"} value="">
-            {(filter == "classification") ? "object type" : filter}
-          </option>
-          {Object.keys(props.startFilters[filter]).map((selectOption, index) => (
-            <option key={index} value={props.startFilters[filter][selectOption].name}>
-              {
-                props.startFilters[filter][selectOption].name + 
-                " (" + props.startFilters[filter][selectOption].objectcount + 
-                ")"
-              }
-            </option>
-          ))}
-        </select>
-      ))}
-    </div>
+  const currentFilters = { century: {}, culture: {}, classification: {} };
+  console.log("filter mounted");
+  console.log(
+    "search value:",
+    props.searchValue,
+    "filter value:",
+    props.filterValue,
+    "currentFilters:", 
+    currentFilters
   );
+
+  props.paintings.forEach((result) => {
+    Object.keys(currentFilters).forEach((filter) => {
+      if (currentFilters[filter][result[filter]] != undefined) {
+        currentFilters[filter][result[filter]]++;
+      } else {
+        currentFilters[filter][result[filter]] = 1;
+      }
+    });
+  });
+
+  if (props.searchValue || Object.keys(props.filterValue).length) {
+    // not a blank search
+    return(
+        <div>
+        {Object.keys(currentFilters).map((filter, index) => (
+            <select
+            key={index}
+            name={filter}
+            onChange={(event) => {
+                props.setFilterValue(prevState => (
+                    {...prevState, [filter]: event.target.value}));
+            }}
+            >
+            <option key={"default"} value="">
+                {filter == "classification" ? "object type" : filter}
+            </option>
+            {Object.keys(currentFilters[filter]).map(
+                (selectOption, index) => (
+                <option
+                    key={index}
+                    value={selectOption}>
+                    {
+                    selectOption +
+                    " (" +
+                    currentFilters[filter][selectOption] +
+                    ")"}
+                </option>
+                )
+            )}
+            </select>
+        ))}
+        </div>
+    )
+
+  } else {
+    // blank search
+    return (
+      <div>
+        {Object.keys(props.startFilters).map((filter, index) => (
+          <select
+            key={index}
+            name={filter}
+            onChange={(event) => {
+                props.setFilterValue(prevState => (
+                    {...prevState, [filter] : event.target.value}));
+            }}
+          >
+            <option key={"default"} value="">
+              {filter == "classification" ? "object type" : filter}
+            </option>
+            {Object.keys(props.startFilters[filter]).map(
+              (selectOption, index) => (
+                <option
+                  key={index}
+                  value={props.startFilters[filter][selectOption].name}
+                >
+                  {props.startFilters[filter][selectOption].name +
+                    " (" +
+                    props.startFilters[filter][selectOption].objectcount +
+                    ")"}
+                </option>
+              )
+            )}
+          </select>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default Filter;
